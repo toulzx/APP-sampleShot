99604d6759fdbed9d75d1dc2725e088e4ec24a13
toulzx <toulzx2021@gmail.com>
2021-09-09 23:11:49 +0800

    bug fixed and README updated

---
+++ README.md
@@ -0,0 +1,31 @@
+# Sample Shot APP
+
+## Summary
+
+Custom camera APP (Native) developed in JAVA, which works properly with Android 10 and Android 11.
+
+## Description
+
+### Function
+
+Take rectangular photos and save.
+
+The aspect ratio of photos can be modified at any time as needed.
+
+### Dependencies
+
+#### compileSdkVersion
+
+`29`
+
+#### Camerax_version
+
+`1.0.0-alpha05`
+
+#### gradle
+
+`com.android.tools.build:gradle:4.1.3`
+
+## Thanks
+
+GitHub : )
--- app/src/main/java/cn/njupt/cl/sampleShot/TakePhotoActivity.java
+++ app/src/main/java/cn/njupt/cl/sampleShot/TakePhotoActivity.java
@@ -11,8 +11,12 @@ import androidx.camera.core.ImageCaptureConfig;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.core.PreviewConfig;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.graphics.Matrix;
 import android.media.Image;
 import android.os.Bundle;
@@ -38,6 +42,15 @@ import static android.content.ContentValues.TAG;
 public class TakePhotoActivity extends AppCompatActivity {
 
     private static final float DEFAULT_ASPECT_RATIO = 3.395f;
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static final int REQUEST_CAMERA = 3;
+
+    private static final String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     private float aspectRatio;
 
     final String format = "%.1f";
@@ -58,20 +71,50 @@ public class TakePhotoActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_takephoto);
 
+        permissionRequest();
+
         setUI();
 
     }
 
+    /**
+     * 动态申请权限
+     * @return void
+     *
+     */
+    private void permissionRequest() {
 
+        // 内存读取权限申请
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+
+        // 相机权限申请
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // You can directly ask for the permission.
+            requestPermissions(new String[] { Manifest.permission.CAMERA }, REQUEST_CAMERA);
+        }
+
+    }
+
+
+    /**
+     * 初始化界面
+     * @return void
+     *
+     */
     private void setUI () {
 
         viewFinder = findViewById(R.id.view_finder);
         btnShot = findViewById(R.id.btn_shot);
         seekBar = findViewById(R.id.seekBar);
         textViewRatio = findViewById(R.id.tv_ratio);
-
         overlayerView = findViewById(R.id.overlayerView);
-
 
         viewFinder.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
             @Override
@@ -108,7 +151,7 @@ public class TakePhotoActivity extends AppCompatActivity {
                 overlayerView.resetAspectRatio(aspectRatio);
                 overlayerView.bringToFront();
                 overlayerView.setCenterRect();
-                overlayerView.invalidate();     // 刷新一下，使获得了宽高后再次调用 onDraw
+                overlayerView.invalidate();     // 刷新一下，使重新调用 onDraw()
                 }
 
             @Override
@@ -127,12 +170,15 @@ public class TakePhotoActivity extends AppCompatActivity {
     /*----------------------------------------------------------------------------------------------------------------*/
 
     private void updateTransform() {
+
         Matrix matrix = new Matrix();
+
         // Compute the center of the view finder
         float centerX = viewFinder.getWidth() / 2f;
         float centerY = viewFinder.getHeight() / 2f;
 
         float[] rotations = {0,90,180,270};
+
         // Correct preview output to account for display rotation
         float rotationDegrees = rotations[viewFinder.getDisplay().getRotation()];
 
@@ -140,23 +186,29 @@ public class TakePhotoActivity extends AppCompatActivity {
 
         // Finally, apply transformations to our TextureView
         viewFinder.setTransform(matrix);
+
     }
 
+    /**
+     * 启动相机实现界面实时预览
+     * @return void
+     *
+     */
     private void startCamera () {
 
-        CameraX.unbindAll();// 从生命周期中解除所有用例的绑定
+        CameraX.unbindAll();                // 从生命周期中解除所有用例的绑定
 
         // 1 preview
         PreviewConfig previewConfig = new PreviewConfig.Builder()
                 .setLensFacing(CameraX.LensFacing.BACK)
                 .build();
-        Preview preview = new Preview(previewConfig);// realTime 预览框实时更新
+        Preview preview = new Preview(previewConfig);
         realTimePreview( preview );
 
 
-        // 2. capture
+        // 2 capture
         ImageCaptureConfig imageCaptureConfig = new ImageCaptureConfig.Builder()
-                .setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY) // 优化捕获管道以优先考虑延迟而不是图像质量。当捕获模式设置为 MIN_LATENCY 时，图像捕获速度可能会更快，但图像质量可能会降低
+                .setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)           // 优化捕获管道以优先考虑图像质量而不是延迟。
                 .build();
 
         final ImageCapture imageCapture = new ImageCapture(imageCaptureConfig);
@@ -165,7 +217,7 @@ public class TakePhotoActivity extends AppCompatActivity {
         takePhotoAction( imageCapture );
 
 
-        // 3. analyze
+        // 3 analyze
         HandlerThread handlerThread = new HandlerThread("Analyze-thread");
         handlerThread.start();
         ImageAnalysisConfig imageAnalysisConfig = new ImageAnalysisConfig.Builder()
@@ -214,7 +266,6 @@ public class TakePhotoActivity extends AppCompatActivity {
         public void analyze(ImageProxy imageProxy, int rotationDegrees) {
             final Image image = imageProxy.getImage();
             if(image != null) {
-                Log.d("尺寸", image.getWidth() + "," + image.getHeight());        // 实时欸
                 // 这里写自己的处理
                 // 考虑留此处接口用以环境检测
             }
@@ -223,13 +274,40 @@ public class TakePhotoActivity extends AppCompatActivity {
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
+    /**
+     * 裁剪并保存照片
+     * @param imageCapture:
+     * @return void
+     *
+     */
     private void saveImage(ImageCapture imageCapture) {
+
         Log.i(TAG, "saveImage: imageCapture.toString()="+ imageCapture.toString());
-        File externalFilesDir = getExternalFilesDir(Environment.DIRECTORY_DCIM);        // 一般放一些长时间保存的数据
 
         long currentTime = System.currentTimeMillis();
-        String storePath = externalFilesDir + File.separator + currentTime + ".jpeg";
 
+//        /* 存放在内部存储 */
+//
+//        File externalFilesDir = getExternalFilesDir(Environment.DIRECTORY_DCIM);
+//        String storePath = externalFilesDir + File.separator + currentTime + ".jpeg";
+
+        /* 存放在外部存储 */
+
+        File file =  new File(Environment.getExternalStorageDirectory()+ File.separator + Environment.DIRECTORY_DCIM);
+
+        String storePath = file + File.separator + "sampleShot" + File.separator  + currentTime + ".jpeg";
+
+        if (!file.exists()) {
+
+            if (!file.mkdirs()) {
+
+                Log.i(TAG, "saveImage: 创建文件夹失败！");
+                
+            }
+            
+        }
+
+        // 目前使用了外部存储地址
         File photo = new File(storePath);
 
         /* 对照片进行裁剪处理 */
@@ -238,10 +316,10 @@ public class TakePhotoActivity extends AppCompatActivity {
 
         imageCapture.setTargetAspectRatio(new Rational((int) (aspectRatio*10),10)); 
 
-        /* 注意这里似乎不能二次旋转，而裁切似乎又必须占满宽，因此在取照片识别之前记得旋转 */
-
+        /* 注意这里似乎不能二次旋转，而裁切要求占满宽度，因此在取照片后处理之前记得旋转 */
 
         /* 保存照片 */
+
         imageCapture.takePicture(photo, new ImageCapture.OnImageSavedListener() {
             @Override
             public void onImageSaved(@NonNull File file) {
@@ -259,7 +337,12 @@ public class TakePhotoActivity extends AppCompatActivity {
 
     /*----------------------------------------------------------------------------------------------------------------*/
 
-
+    /**
+     * 底部悬浮提示
+     * @param msg:  提示内容
+     * @return void
+     *
+     */
     public void showToast(String msg) {
 
         Toast.makeText(TakePhotoActivity.this, msg, Toast.LENGTH_SHORT).show();
